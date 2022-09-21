d4c7b78bb8cac299f3a07192cafee69f247fe35e
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-21 14:57:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,10 @@ if [ -z "$GCP_SERVICE_ACCOUNT_KEY_FILE" ]; then
   exit 1
 fi
 
-echo $GCP_SERVICE_ACCOUNT_KEY_FILE > json_file.json
+sudo apt install jq
+
+jq -n $GCP_SERVICE_ACCOUNT_KEY_FILE > json_file.json
+
 gcloud auth activate-service-account --key-file json_file.json
 
 sh -c "gsutil cp gs://${GCP_STORAGE_BUCKET}/${DEST_DIR} ${SOURCE_DIR}"
