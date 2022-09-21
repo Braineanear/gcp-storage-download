3f19238e663f636f5d962a939de9321fcb9507f1
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-21 16:54:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 echo '$GCP_SERVICE_ACCOUNT_KEY_FILE' > json_file.json
 
+var = $(base64 json_file.json)
+
+echo $var > json_file.json
+
 gcloud auth activate-service-account --key-file json_file.json
 
 sh -c "gsutil cp gs://${GCP_STORAGE_BUCKET}/${DEST_DIR} ${SOURCE_DIR}"
