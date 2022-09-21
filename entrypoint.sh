f08125894d561a389fae63e483e5b5b7c36e6b15
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-21 16:24:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-apt install jq
-
-echo "$GCP_SERVICE_ACCOUNT_KEY_FILE" | base64 -d > json_file.json
+echo '$GCP_SERVICE_ACCOUNT_KEY_FILE' > json_file.json
 
 gcloud auth activate-service-account --key-file json_file.json
 
